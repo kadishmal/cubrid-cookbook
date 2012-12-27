7862661873d99ec0ab5828b4d1c2c504af04aff6
Esen Sagynov <kadishmal@gmail.com>
2012-12-27 22:08:53 +0900

    pdo_cubrid: install from pecl instead of manually downloading the source code.

--- attributes/pdo_cubrid.rb
+++ attributes/pdo_cubrid.rb
@@ -7,24 +7,10 @@
 # Distributed under MIT license
 #
 
-# the version of CUBRID to install
-default['cubrid']['version'] = "9.0.0"
-# the home directory of a Vagrant user
-default['cubrid']['user_home_dir'] = "/home/vagrant"
-# the directory where CUBRID is installed
-default['cubrid']['home'] = "/opt/cubrid"
+# the name of a PECL package to install CUBRID PDO driver
+default['cubrid']['pdo_package'] = "PDO_CUBRID-#{node['cubrid']['version']}.0001"
 
-default['cubrid']['pdo_version'] = "#{default['cubrid']['version']}.0001"
-# the name of a directory where PDO driver source code is extracted
-default['cubrid']['pdo_dirname'] = "PDO_CUBRID-#{default['cubrid']['pdo_version']}"
-# the file name of the archive to download
-default['cubrid']['pdo_filename'] = "#{default['cubrid']['pdo_dirname']}.tgz"
-# the full URL of the TAR archive to download
-default['cubrid']['pdo_url'] = "http://pecl.php.net/get/#{default['cubrid']['pdo_filename']}"
-
-# the full path of a directory where pdo_cubrid.ini should be placed
-default['cubrid']['pdo_ext_conf_dir'] = '/etc/php5/conf.d'
 # the full path of pdo_cubrid.ini
-default['cubrid']['pdo_ext_conf'] = "#{default['cubrid']['pdo_ext_conf_dir']}/pdo_cubrid.ini"
+default['cubrid']['pdo_ext_conf'] = "/etc/php5/conf.d/pdo_cubrid.ini"
 # the directives which should be placed in pdo_cubrid.ini files; these are populate to pdo_cubrid.ini.erb template of this cookbook.
 default['cubrid']['pdo_directives'] = {:extension => "pdo_cubrid.so"}
--- recipes/pdo_cubrid.rb
+++ recipes/pdo_cubrid.rb
@@ -11,48 +11,7 @@ include_recipe "php"
 include_recipe "cubrid"
 include_recipe "apache2"
 
-USER_HOME_DIR = "#{node['cubrid']['user_home_dir']}"
-CUBRID_HOME_DIR = "#{node['cubrid']['home']}"
-
-PDO_FILENAME = "#{node['cubrid']['pdo_filename']}"
-PDO_DIRNAME = "#{node['cubrid']['pdo_dirname']}"
-
-remote_file "#{USER_HOME_DIR}/#{PDO_FILENAME}" do
-  user "vagrant"
-  source "#{node['cubrid']['pdo_url']}"
-  mode 0644
-  not_if "php -i | grep 'Client API version => 9.0.0.0001'"
-end
-
-execute "extract #{PDO_FILENAME}" do
-  user "vagrant"
-  cwd "#{USER_HOME_DIR}"
-  command "tar -zxf #{PDO_FILENAME}"
-  not_if "php -i | grep 'Client API version => 9.0.0.0001'"
-end
-
-file "#{USER_HOME_DIR}/#{PDO_FILENAME}" do
-  action :delete
-  not_if "php -i | grep 'Client API version => 9.0.0.0001'"
-  backup false
-end
-
-execute "configure phpize for #{PDO_DIRNAME}" do
-  cwd "#{USER_HOME_DIR}/#{PDO_DIRNAME}"
-  command "phpize"
-  not_if "php -i | grep 'Client API version => 9.0.0.0001'"
-end
-
-execute "configure #{PDO_DIRNAME}" do
-  cwd "#{USER_HOME_DIR}/#{PDO_DIRNAME}"
-  command "./configure --with-pdo-cubrid=#{CUBRID_HOME_DIR}"
-  not_if "php -i | grep 'Client API version => 9.0.0.0001'"
-end
-
-execute "install #{PDO_DIRNAME}" do
-  user "root"
-  cwd "#{USER_HOME_DIR}/#{PDO_DIRNAME}"
-  command "make && make install"
+execute "echo '#{node['cubrid']['home']}' | pecl install #{node['cubrid']['pdo_package']}" do
   not_if "php -i | grep 'Client API version => 9.0.0.0001'"
 end
 
@@ -65,20 +24,6 @@ template "#{node['cubrid']['pdo_ext_conf']}" do
   not_if "php -i | grep 'Client API version => 9.0.0.0001'"
 end
 
-directory "#{USER_HOME_DIR}/#{PDO_DIRNAME}" do
-  user "root"
-  recursive true
-  action :delete
-  not_if "php -i | grep 'Client API version => 9.0.0.0001'"
-end
-
-file "#{USER_HOME_DIR}/package.xml" do
-  user "root"
-  action :delete
-  not_if "php -i | grep 'Client API version => 9.0.0.0001'"
-end
-
 service "apache2" do
   action :restart
-  not_if "php -i | grep 'Client API version => 9.0.0.0001'"
 end
