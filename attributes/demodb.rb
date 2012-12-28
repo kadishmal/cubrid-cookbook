4d332c6a7e4de6b2b3d16e8ad0aad0a2075b12ee
Esen Sagynov <kadishmal@gmail.com>
2012-12-28 12:13:40 +0900

    Cannot access attribtues from "default" recipe, so need to define necessary attributes again.

--- attributes/demodb.rb
+++ attributes/demodb.rb
@@ -6,8 +6,10 @@
 #
 # Distributed under MIT license
 #
+# the default target directory to install CUBRID
+default['cubrid']['home'] = "/opt/cubrid"
 
 # the directory where to install the demodb database
-default['cubrid']['demodb_dir'] = "#{node['cubrid']['home']}/databases/demodb"
+set['cubrid']['demodb_dir'] = "#{node['cubrid']['home']}/databases/demodb"
 # the full path of a script which install the demodb database
-default['cubrid']['demodb_script'] = "#{node['cubrid']['home']}/demo/make_cubrid_demo.sh"
+set['cubrid']['demodb_script'] = "#{node['cubrid']['home']}/demo/make_cubrid_demo.sh"
--- attributes/pdo_cubrid.rb
+++ attributes/pdo_cubrid.rb
@@ -6,11 +6,13 @@
 #
 # Distributed under MIT license
 #
+# the default version of CUBRID to install
+default['cubrid']['version'] = "9.0.0"
 
 # the name of a PECL package to install CUBRID PDO driver
-default['cubrid']['pdo_package'] = "PDO_CUBRID-#{node['cubrid']['version']}.0001"
+set['cubrid']['pdo_package'] = "PDO_CUBRID-#{node['cubrid']['version']}.0001"
 
 # the full path of pdo_cubrid.ini
-default['cubrid']['pdo_ext_conf'] = "/etc/php5/conf.d/pdo_cubrid.ini"
+set['cubrid']['pdo_ext_conf'] = "/etc/php5/conf.d/pdo_cubrid.ini"
 # the directives which should be placed in pdo_cubrid.ini files; these are populate to pdo_cubrid.ini.erb template of this cookbook.
-default['cubrid']['pdo_directives'] = {:extension => "pdo_cubrid.so"}
+set['cubrid']['pdo_directives'] = {:extension => "pdo_cubrid.so"}
