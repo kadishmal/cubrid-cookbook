e4d9f242f607c0a5377491f7a23d0b7d53747e68
Esen Sagynov <kadishmal@gmail.com>
2013-01-04 13:04:05 +0900

    New: added CUBRID HA support through "ha" recipe.

---
+++ attributes/ha.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: cubrid
+# Attributes:: ha
+#
+# Copyright 2012, Esen Sagynov <kadishmal@gmail.com>
+#
+# Distributed under MIT license
+#
+
+include_attribute "cubrid::database"
+
+#######################################################
+# the following are configurations for cubrid_ha.conf #
+#######################################################
+
+# a default list of databases to create and configure for CUBRID HA
+default['cubrid']['ha_dbs'] = ["testdb"]
+# the name of the HA group
+default['cubrid']['ha_group'] = "cubrid"
+# a default list of hosts to join ha_group
+# in the form of { "node1" => "IP 1", "node2" => "IP 2" ... }
+default['cubrid']['ha_hosts'] = {}
+
+HA_DB_LIST = ""
+HA_HOSTS = ""
+
+node['cubrid']['ha_dbs'].each do |db|
+	HA_DB_LIST << (HA_DB_LIST == "" ? "" : ",") << db
+end
+
+node['cubrid']['ha_hosts'].each do |host, ip|
+	HA_HOSTS << (HA_HOSTS == "" ? "" : ":") << host
+end
+
+# ha_db_list in the form of db1,db2...
+set['cubrid']['ha_db_list'] = HA_DB_LIST
+# ha_hosts_list in the form of node1:node2...
+set['cubrid']['ha_hosts_list'] = HA_HOSTS
+# ha_node_list in the form of ha_group@ha_hosts
+set['cubrid']['ha_node_list'] = "#{node['cubrid']['ha_group']}@#{HA_HOSTS}"
+
+# the configurations directory
+set['cubrid']['conf_dir'] = "#{node['cubrid']['home']}/conf"
+# full path to cubrid.conf
+set['cubrid']['conf'] = "#{node['cubrid']['conf_dir']}/cubrid.conf"
+# full path to cubrid_ha.conf
+set['cubrid']['ha_conf'] = "#{node['cubrid']['conf_dir']}/cubrid_ha.conf"
---
+++ recipes/ha.rb
@@ -0,0 +1,67 @@
+#
+# Cookbook Name:: cubrid
+# Recipes:: ha
+#
+# Copyright 2012, Esen Sagynov <kadishmal@gmail.com>
+#
+# Distributed under MIT license
+#
+
+require 'chef/util/file_edit'
+
+include_recipe "cubrid"
+
+if !node['cubrid']['ha_hosts'].empty?
+	CUBRID_CONF = "#{node['cubrid']['conf']}"
+	HA_CONF = "#{node['cubrid']['ha_conf']}"
+	DATABASES_TXT = "#{node['cubrid']['databases_dir']}/databases.txt"
+
+	# create HA databases
+	node['cubrid']['ha_db_list'].each do |db|
+		cubrid_database db do
+			action :create
+			autostart false
+		end
+
+		ruby_block "add hosts to databases.txt for #{db} database" do
+      block do
+        databases_txt = Chef::Util::FileEdit.new("#{DATABASES_TXT}")
+				databases_txt.search_file_replace(/#{db}[\t\s]+localhost/, "#{db}	#{node['cubrid']['ha_hosts_list']}")
+				databases_txt.write_file
+      end
+      only_if "egrep '#{db}[\t\s]+localhost' #{DATABASES_TXT}"
+    end
+	end
+
+	node['cubrid']['ha_hosts'].each do |host, ip|
+		execute "sudo sed -i '$a #{ip} #{host}' /etc/hosts" do
+			not_if "egrep '\b#{host}\b' /etc/hosts"
+		end
+	end
+
+	# update cubrid_ha.conf
+	template HA_CONF do
+	  source "cubrid_ha.conf.erb"
+	  user "vagrant"
+	  not_if "cat #{HA_CONF} | grep 'Cookbook Name:: cubrid'"
+	end
+
+	# update cubrid.conf
+	template CUBRID_CONF do
+	  source "cubrid.conf.erb"
+	  user "vagrant"
+	  not_if "cat #{CUBRID_CONF} | grep 'Cookbook Name:: cubrid'"
+	end
+
+	# restart cubrid service
+	execute "cubrid service restart" do
+	  user "vagrant"
+	end
+
+	# start cubrid heartbeat
+	execute "cubrid heartbeat start" do
+	  user "vagrant"
+	end
+else
+	raise Chef::Exceptions::AttributeNotFound, "Cannot configure CUBRID HA without ha_hosts. Refer to \"ha_hosts\" attribute in /cubrid/attributes/ha.rb for the syntax."
+end
