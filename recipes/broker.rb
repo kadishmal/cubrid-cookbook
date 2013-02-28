4e83cd52e39cdf93f69e78a8f92fd5c1df304a21
Esen Sagynov <kadishmal@gmail.com>
2013-02-28 20:06:27 +0900

    Enh: restart CUBRID Broker Service only if its configuration file is updated.

--- recipes/broker.rb
+++ recipes/broker.rb
@@ -10,11 +10,19 @@ include_recipe "cubrid"
 
 BROKER_CONF = node['cubrid']['broker_conf']
 
+# Stop CUBRID Broker Service.
+# I have specifically separated this command into stop/start
+# instead of restart because I don't want to restart the
+# Broker Service if its configuration file will not be udpated.
+execute "cubrid broker stop" do
+  not_if "cat #{BROKER_CONF} | grep 'Cookbook Name:: cubrid'"
+end
+
 # update cubrid.conf
 template BROKER_CONF do
   source "broker.cubrid_broker.conf.erb"
   not_if "cat #{BROKER_CONF} | grep 'Cookbook Name:: cubrid'"
 end
 
-# Start CUBRID Service.
-execute "cubrid broker restart"
+# Start CUBRID Broker Service.
+execute "cubrid broker start"
