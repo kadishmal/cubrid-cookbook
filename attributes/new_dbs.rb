5d4a51a03d0907e42c42a5bfe9b7adfce4a3420d
Esen Sagynov <kadishmal@gmail.com>
2013-02-28 20:09:13 +0900

    Enh: allow to add a new database user with a given password to a newly created database, or reset the initially empty password with a given one.

--- attributes/new_dbs.rb
+++ attributes/new_dbs.rb
@@ -8,5 +8,11 @@
 #
 include_attribute "cubrid::database"
 
-# a default list of databases to create
+# A default list of databases to create.
 default['cubrid']['new_dbs'] = []
+# A default user to create for each database.
+# When empty, 'dba' will be the only administrator user.
+default['cubrid']['new_db_user'] = ''
+# A user password. If `new_db_user_pass` is specified but `new_db_user` is not,
+# the specified password will be set to the default `dba` user.
+default['cubrid']['new_db_user_pass'] = ''
--- recipes/new_dbs.rb
+++ recipes/new_dbs.rb
@@ -11,5 +11,7 @@ include_recipe "cubrid"
 node['cubrid']['new_dbs'].each do |db|
 	cubrid_database db do
 		action :create
+		dbuser node['cubrid']['new_db_user']
+		password node['cubrid']['new_db_user_pass']
 	end
 end
